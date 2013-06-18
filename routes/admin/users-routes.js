f3bce50cdf7ceb7a7e9db43eca030c37326dc4fe
ijin <ilya.rogojin@gmail.com>
2013-06-18 23:51:33 +0300

    change user role

--- routes/admin/users-routes.js
+++ routes/admin/users-routes.js
@@ -1,4 +1,7 @@
 module.exports = function (app, admin) {
   // users list
   app.get('/adm/users/:page_id?', admin.usersList);
+
+  // users list
+  app.get('/adm/set-user-role/:userId/:role?', admin.setUserRole);
 }
--- routes/admin/users.js
+++ routes/admin/users.js
@@ -8,4 +8,38 @@ exports.usersList = function (req, res, next) {
         users: qres
       });
     });
-}
+};
+
+exports.setUserRole = function (req, res, next) {
+
+  // all roles list hardcoded
+  var availRoles = ['user', 'writer', 'admin'];
+
+  // params for change role
+  var userId = check.numeric(req.params.userId);
+  var userRole = (availRoles.indexOf(req.params.role) >= 0) ? req.params.role : null;
+
+  // check current user role
+  if (userRole !== null) {
+    if (req.userInfo.role == 'admin') {
+      db.q("UPDATE users \
+        SET role=? \
+        WHERE user_id=?",
+      [
+        userRole,
+        userId
+      ], function (err, qres) {
+          if (err) return next(err);
+
+          res.json({
+            result: true,
+            role: userRole,
+            userId: userId});
+        });
+    } else {
+      res.json({ result: false});
+    }
+  } else {
+    res.json({ result: false});
+  }
+};
