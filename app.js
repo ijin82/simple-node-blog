4fd877547989ac3ef1a4fcbfb66347f27062a779
ijin82 <ilya.rogojin@gmail.com>
2014-05-05 01:09:02 +0300

    moved to express4 + new mysql-connect for sessions + tiny refact

--- app.js
+++ app.js
@@ -1,54 +1,67 @@
 // local vars
 var express = require('express')
     , app = express()
-    , http = require('http')
     , path = require('path')
     , flash = require('connect-flash')
-    , colors = require('colors');
+    , colors = require('colors')
+    , compress = require('compression')
+    , favicon = require('static-favicon')
+    , morgan = require('morgan') // ex logger
+    , bodyParser = require('body-parser')
+    , methodOverride = require('method-override')
+    , cookieParser = require('cookie-parser')
+    , session = require('express-session')
+    ;
 
 // global vars
     dbConfig = require("./config/dbConfig")
   , dbPool = require('./libs/db')
   , check = require('./libs/check')
-  , dateformat = require('dateformat');
+  , dateformat = require('dateformat')
+  , app_env = process.env.NODE_ENV || 'development';
 
-  dbPool.getConn(function(dbConn){
+dbPool.getConn(function(dbConn){
+  app.set('port', 3737);
 
-    // main global db connect
-    db = dbConn;
+  // main global db connect
+  db = dbConn;
 
-// helpers
-require('./libs/helpers')(app);
+  // helpers
+  require('./libs/helpers')(app);
 
-// main config area
-app.configure(function(){
-  
+    
   // gzip all
-  app.use(express.compress());
+//  app.use(compress);
 
   // error handler
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+  app.use(require('errorhandler')({ dumpExceptions: true, showStack: true }));
 
   // etc settings
-  app.set('port', process.env.COCAINUM_PORT || 3737);
+  app.set('port', process.env.SIMPLE_NODE_BLOG_PORT || 3737);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
-  app.use(express.favicon(__dirname + '/public/favicon.ico'));
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
+  app.use(favicon(__dirname + '/public/favicon.ico'))
+  app.use(morgan({ format: 'dev', immediate: true }));
+  app.use(bodyParser());
+  app.use(methodOverride());
 
   // init session
-  app.use(express.cookieParser('lalaleqwe123111keqwe123123ndfldsaknflsdfsdf'));
+  app.use(cookieParser('lalaleqwe123111keqwe123123ndfldsaknflsdfsdf'));
 
   // sessions mysql store
-  var MySQLSessionStore = require('connect-mysql-session')(express);
-  app.use(express.session({
-    store: new MySQLSessionStore(dbConfig.database, dbConfig.user, dbConfig.password, {
-        defaultExpiration: 5*60*60*24*1000, // 5 days
-        logging: false // log to console switch
-    }),
-    secret: 'adsfjah;f~~~afiuailfasilu123dfgliadgsfib 2348o134t8734t5rqwdekfanfw'}));
+  var MysqlStore = require('connect-mysql')({session: session});
+  app.use(session({
+    secret: 'adhlbq erldhbq ;erb3241434134-~~~asd',
+    store: new MysqlStore({
+      config: {
+        user: dbConfig.user,
+        password: dbConfig.password,
+        database: dbConfig.database
+      }
+    })
+  }));
+
+  //flash messages
   app.use(flash());
 
   // static
@@ -62,9 +75,40 @@ app.configure(function(){
   require('./config/pass-github-conf')(app);
   require('./config/pass-local-conf')(app);
 
-  // router
-  app.use(app.router);
+  // load global app config
+  var settings = require('./routes/admin/settings');
 
+  // global config here
+  appConfig = {};
+  settings.getConf(function(err){
+    if (err) throw err;
+  }); // loaded config
+
+  // auth module
+  require('./routes/auth-routes')(app, require('./routes/auth'));
+
+  // blog module
+  require('./routes/blog-routes')(app, require('./routes/blog'));
+
+  // admin BASE module (main routing check)
+  require('./routes/admin/admin-routes')(app, require('./routes/admin/admin'));
+
+  // admin users module
+  require('./routes/admin/users-routes')(app, require('./routes/admin/users'));
+
+  // admin settings module
+  require('./routes/admin/settings-routes')(app, settings);
+
+  // user zone
+  require('./routes/my-routes')(app, require('./routes/my'));
+
+  // sitemap
+  require('./routes/sitemap-routes')(app, require('./routes/sitemap'));
+
+  // rss
+  require('./routes/rss-routes')(app, require('./routes/rss'));
+
+  ////////////////////////////////////////
   // error 404 (just last available route)
   app.use(function(req, res, next){
 
@@ -74,62 +118,28 @@ app.configure(function(){
     });
   });
 
-});
+  if (app_env == 'development') {
+    app.use(function(err, req, res, next) {
 
-app.configure('development', function(){
-  app.use(function(err, req, res, next) {
-
-    res.status(500).render('err500', {
-      title: 'ouch.. 500 [dev]',
-      err: err
+      res.status(500).render('err500', {
+        title: 'ouch.. 500 [dev]',
+        err: err
+      });
     });
-  });
-})
+  }
 
-app.configure('production', function(){
-  app.use(function(err, req, res, next) {
+  if (app_env == 'production') {
+    app.use(function(err, req, res, next) {
 
-    res.status(500).render('err500', {
-      title: 'ouch.. 500',
-      err: ''
+      res.status(500).render('err500', {
+        title: 'ouch.. 500',
+        err: ''
+      });
     });
-  });
-})
+  }
 
-// load global app config
-var settings = require('./routes/admin/settings');
-
-// global config here
-appConfig = {};
-settings.getConf(function(err){
-  if (err) throw err;
-}); // loaded config
-
-// auth module
-require('./routes/auth-routes')(app, require('./routes/auth'));
-
-// blog module
-require('./routes/blog-routes')(app, require('./routes/blog'));
-
-// admin BASE module (main routing check)
-require('./routes/admin/admin-routes')(app, require('./routes/admin/admin'));
-// admin users module
-require('./routes/admin/users-routes')(app, require('./routes/admin/users'));
-// admin settings module
-require('./routes/admin/settings-routes')(app, settings);
-
-// user zone
-require('./routes/my-routes')(app, require('./routes/my'));
-
-// sitemap
-require('./routes/sitemap-routes')(app, require('./routes/sitemap'));
-
-// rss
-require('./routes/rss-routes')(app, require('./routes/rss'));
-
-// start http server here
-server = http.createServer(app).listen(app.get('port'), function(){
+  // start server here
+  app.listen(app.get('port'));
   console.log("-- simple node blog listening on port " + app.get('port') + " --");
-});
 
 });
--- config/pass-keys.js
+++ config/pass-keys.js
@@ -1,7 +1,7 @@
 module.exports = function (app) {
   // dev & production settings
   // .bashrc -> export NODE_ENV=development
-  app.configure('development', function () {
+  if(app_env == 'development') {
     app.set('GITHUB_APP_ID', 'f676da835969071bb15f');
     app.set('GITHUB_APP_SECRET', '0cc61f25b4c12657badc9bd4e367c82bd9942156');
     app.set('GITHUB_CALLBACK_URL', 'http://127.0.0.1:3737/auth/github/callback');
@@ -10,23 +10,23 @@ module.exports = function (app) {
     app.set('FACEBOOK_APP_SECRET', '435903f8f94557a2622d0d8bb65dfdb1');
     app.set('FACEBOOK_CALLBACK_URL', 'http://127.0.0.1:3737/auth/facebook/callback');
 
-    app.set('TWITTER_APP_ID', 'rwhDFFDhbi9NcPCkAKWqoQ');
-    app.set('TWITTER_APP_SECRET', 'RBdkPAWKlQMxw6khisiw6IA6AXpUKCSOe988qBK0yGY');
+    app.set('TWITTER_APP_ID', '65uycMResbJpt1AXha2WgA80j');
+    app.set('TWITTER_APP_SECRET', 'ijATUz3B2GgdOtky8CV1g6S3Nxy1rMgM8MjlrDQ4naikj6XxQT');
     app.set('TWITTER_CALLBACK_URL', 'http://127.0.0.1:3737/auth/twitter/callback');
-  });
+  }
 
   // .bashrc -> export NODE_ENV=production
-  app.configure('production', function () {
+  if(app_env == 'production') {
     app.set('GITHUB_APP_ID', 'c5c88f8112aa2e4fabb8');
     app.set('GITHUB_APP_SECRET', 'b2b8060bb64b71b895b5c43de06403d94328a6df');
-    app.set('GITHUB_CALLBACK_URL', 'http://demo.cocainum.info/auth/github/callback');
+    app.set('GITHUB_CALLBACK_URL', 'http://snb-demo.wired-mind.info/auth/github/callback');
 
     app.set('FACEBOOK_APP_ID', '370319946409714');
     app.set('FACEBOOK_APP_SECRET', '8f03fdbd94c1f0e82884ac504a09133e');
-    app.set('FACEBOOK_CALLBACK_URL', 'http://demo.cocainum.info/auth/facebook/callback');
+    app.set('FACEBOOK_CALLBACK_URL', 'http://snb-demo.wired-mind.info/auth/facebook/callback');
 
     app.set('TWITTER_APP_ID', 'wKR7XhzfT29STBjnw6xA');
     app.set('TWITTER_APP_SECRET', 'f1JdJTlbIMpug7r7fRcUiyzQM8XfmvqthWrAtHkYc');
-    app.set('TWITTER_CALLBACK_URL', 'http://demo.cocainum.info/auth/twitter/callback');
-  });
+    app.set('TWITTER_CALLBACK_URL', 'http://snb-demo.wired-mind.info/auth/twitter/callback');
+  }
 }
