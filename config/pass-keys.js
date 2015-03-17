aba8aa3f94f191dad507fe27e1e0ffc372c8dfb7
ijin82 <ilya.rogojin@gmail.com>
2015-03-18 00:44:40 +0300

    new twitter app for demo + dep updates

--- config/pass-keys.js
+++ config/pass-keys.js
@@ -10,8 +10,8 @@ module.exports = function (app) {
     app.set('FACEBOOK_APP_SECRET', '435903f8f94557a2622d0d8bb65dfdb1');
     app.set('FACEBOOK_CALLBACK_URL', 'http://127.0.0.1:3737/auth/facebook/callback');
 
-    app.set('TWITTER_APP_ID', '65uycMResbJpt1AXha2WgA80j');
-    app.set('TWITTER_APP_SECRET', 'ijATUz3B2GgdOtky8CV1g6S3Nxy1rMgM8MjlrDQ4naikj6XxQT');
+    app.set('TWITTER_APP_ID', 'J3F5ozBPLBbAg7BW0f1AfG20G');
+    app.set('TWITTER_APP_SECRET', 'kelJsfZljGsTeh60uBneNg3PapZ3ifkzzNoOC2wuWBfSxzLlPn');
     app.set('TWITTER_CALLBACK_URL', 'http://127.0.0.1:3737/auth/twitter/callback');
   }
 
@@ -25,8 +25,8 @@ module.exports = function (app) {
     app.set('FACEBOOK_APP_SECRET', '8f03fdbd94c1f0e82884ac504a09133e');
     app.set('FACEBOOK_CALLBACK_URL', 'http://snb-demo.wired-mind.info/auth/facebook/callback');
 
-    app.set('TWITTER_APP_ID', 'OaZVyDuMeWedlsY9xnWnEm5ik');
-    app.set('TWITTER_APP_SECRET', 'UMJvjefNxElseD43jfyMcxTiAxMIsDZHFtbNVAOR40aHEbaCeU');
+    app.set('TWITTER_APP_ID', 'wMhusmgb9sywSHaeMw5MoxX9X');
+    app.set('TWITTER_APP_SECRET', 'AfbDAdexZWKEudRRBhEfb47oZCMP1n6IEtvITDYz50T9Rka35G');
     app.set('TWITTER_CALLBACK_URL', 'http://snb-demo.wired-mind.info/auth/twitter/callback');
   }
 }
